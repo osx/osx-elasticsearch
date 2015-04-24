f06c0764b18bb0b5751935f32408c0d051f94438
Greg Levine <levine.greg@gmail.com>
2015-04-24 14:43:18 -0400

    refactored lwrp
    - The resource has the method `exists?` instead of an attribute.
    - The resource name is set automatically by Chef when the current resource is loaded.

--- providers/plugin.rb
+++ providers/plugin.rb
@@ -10,7 +10,7 @@ end
 use_inline_resources
 
 action :install do
-  if @current_resource.exists
+  if @current_resource.exists?
     Chef::Log.info "#{@new_resource} is already installed"
   else
     converge_by("install plugin: #{@new_resource}") do
@@ -20,7 +20,7 @@ action :install do
 end
 
 action :uninstall do
-  if @current_resource.exists
+  if @current_resource.exists?
     converge_by("uninstall plugin: #{@new_resource}") do
       uninstall_plugin @new_resource.name
     end
@@ -31,8 +31,6 @@ end
 
 def load_current_resource
   @current_resource = Chef::Resource::OsxElasticsearchPlugin.new @new_resource.name
-  @current_resource.name(@new_resource.name)
-  @current_resource.exists = plugin_exists? @new_resource.name
 end
 
 def install_plugin name
@@ -47,17 +45,6 @@ def uninstall_plugin name
     command "plugin --remove #{name}"
     cwd "#{bin_dir}"
   end
-end
-
-def plugin_exists? name
-  homebrew = shell_out('brew --prefix').stdout.chomp
-  dir = "#{homebrew}/var/lib/elasticsearch/plugins/"
-  Chef::Log.debug "Looking for #{name} in #{dir}"
-
-  Dir.entries(dir).any? do |plugin|
-    next if plugin =~ /^\./
-    name.include? plugin
-  end rescue false
 end
 
 def bin_dir
--- resources/plugin.rb
+++ resources/plugin.rb
@@ -1,3 +1,5 @@
+include Chef::Mixin::ShellOut
+
 actions :install, :uninstall
 default_action :install
 
@@ -5,4 +7,13 @@ provides :osx_elasticsearch_plugin, os: 'mac_os_x'
 
 attribute :name, :kind_of => String, :name_attribute => true
 
-attr_accessor :exists
+def exists?
+  homebrew = shell_out('brew --prefix').stdout.chomp
+  dir = "#{homebrew}/var/lib/elasticsearch/plugins/"
+  Chef::Log.debug "Looking for #{@name} in #{dir}"
+
+  Dir.entries(dir).any? do |plugin|
+    next if plugin =~ /^\./
+    @name.include? plugin
+  end rescue false
+end
