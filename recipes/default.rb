5c3a80949ec4e73be7b4ac0e0ad383c0585feee8
Greg Levine <levine.greg@gmail.com>
2015-04-26 03:13:40 -0400

    launch Elasticsearch at login
    Can't run two service calls because of https://github.com/chef/chef/issues/3285. So the symlink is created and the service is started after all of the plugins are installed. This guarantees that Elasticsearch and its plugins will be installed before attempting to start the service. But it also means that the service cannot be restarted by any other recipe or resource.
    
    Resolves #2.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,11 @@
 package 'elasticsearch'
 
 include_recipe 'osx-elasticsearch::plugins'
+
+link "#{ENV['HOME']}/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist" do
+  to '/usr/local/opt/elasticsearch/homebrew.mxcl.elasticsearch.plist'
+end
+
+service 'homebrew.mxcl.elasticsearch' do
+  action :enable
+end
